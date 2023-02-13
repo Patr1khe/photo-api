1ebec8b86fafc94e5d6bd3b528f2867a409694b9
Patrik He <patrikswgit@gmail.com>
2023-02-13 20:00:48 +0100

    imported & added router.use for photos, login and profile. LoginUserRules doesnt work atm. validatetoken work for profile and createUserRules also working for register

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import express from "express"
 import resource from './_router'
+import profile from './profile'
+import photos from './photos'
+import { register, refresh, login } from '../controllers/user_controller'
+import { createUserRules, loginUserRules } from "../validations/user_rules"
+import { validateToken } from "../middlewares/auth/jwt"
 
 // instantiate a new router
 const router = express.Router()
@@ -14,8 +19,30 @@ router.get('/', (req, res) => {
 })
 
 /**
- * [EXAMPLE] /resource
+ * /photos
  */
-// router.use('/resource', resource)
+router.use('/photos', photos)
+
+
+
+/**
+ * POST /login
+ */
+router.use('/login', loginUserRules, login)
+
+/**
+ * POST /refresh
+ */
+router.post('/refresh', refresh)
+
+/**
+ * /profile
+ */
+router.use('/profile', validateToken, profile)
+
+/**
+ * /register
+ */
+router.post('/register', createUserRules, register)
 
 export default router
