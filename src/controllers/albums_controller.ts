5a4b15172ae6ac2ebd5a94a05336fd39bd1b50b5
Patrik He <patrikswgit@gmail.com>
2023-02-16 00:17:39 +0100

    added addphoto fuction for single photo to add in album and also added addphotos function for multiple photos to add in album.
    even added updateAlbumId functions to change title for album on postman

--- src/controllers/albums_controller.ts
+++ src/controllers/albums_controller.ts
@@ -90,3 +90,116 @@ export const store = async (req: Request, res: Response) => {
     }
 }
 
+/**
+ * Add a photo to an album
+ */
+export const addPhoto = async (req: Request, res: Response) => {
+    const validationFails = validationResult(req)
+    if (!validationFails.isEmpty()) {
+        return res.status(400).send({
+            status: "fail",
+            data: validationFails.array(),
+        })
+    }
+ 
+    const albumId = Number(req.params.albumId)
+    debug(req.token)
+    
+    try {
+        const album = await prisma.album.update({
+            where: {
+                id: albumId,
+            },
+            data: {
+                photos: {
+                    connect: {
+                        id: req.body.photoId,
+                    }
+                }
+            },
+            include: {
+                photos: true,
+            }
+        })
+        debug(album)
+        res.status(201).send({ status: "success", data: album })
+    } catch (err) {
+        debug(err)
+        return res.status(500).send({ status: "error", message: "Could not create album in database"})
+    }
+}
+
+/**
+ * Add multiple photos to an album
+ */
+export const addPhotos = async (req: Request, res: Response) => {
+    const validationFails = validationResult(req)
+    if (!validationFails.isEmpty()) {
+        return res.status(400).send({
+            status: "fail",
+            data: validationFails.array(),
+        })
+    }
+
+    const photoIds = req.body.photoIds.map( (photoId: Number) => {
+        return {
+            id: photoId,
+        }
+    })  
+ 
+
+    const albumId = Number(req.params.albumId)
+    debug(req.token)
+    
+    try {
+        const album = await prisma.album.update({
+            where: {
+                id: albumId,
+            },
+            data: {
+                photos: {
+                    connect: photoIds
+                }
+            },
+            include: {
+                photos: true,
+            }
+        })
+        debug(album)
+        res.status(201).send({ status: "success", data: album })
+    } catch (err) {
+        debug(err)
+        return res.status(500).send({ status: "error", message: "Could not create album in database"})
+    }
+}
+
+
+
+/**
+ * Update a existing album
+ */
+export const updateAlbumId = async (req: Request, res: Response) => {
+    // check Errors for any validation
+    const validationFails = validationResult(req)
+    if (!validationFails.isEmpty()) {
+        return res.status(400).send({
+            status: "fail",
+            data: validationFails.array(),
+        })
+    }
+    const albumId = Number(req.params.albumId)
+
+    try {
+        const album = await prisma.album.update({
+            where: {
+                id: albumId,
+            },
+            data: req.body
+        })
+        debug(album)
+        res.send({ status: "success", data: album})
+    } catch (err) {
+        debug(err)
+        return res.status(500).send({ status: "error", message: "Could not update photo in database"})
+    }
+}
