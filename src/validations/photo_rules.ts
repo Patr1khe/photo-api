03fed9589e6b21a74c1f31cf812724bd01ec86bc
Patrik He <patrikswgit@gmail.com>
2023-02-17 13:59:36 +0100

    removed extra validation rule for comment

--- src/validations/photo_rules.ts
+++ src/validations/photo_rules.ts
@@ -3,21 +3,11 @@ import { body } from 'express-validator'
 export const createPhotoRules = [
     body('title').isString().withMessage('Has to be a string').trim().isLength({min: 3}).withMessage('has to be atleast 3 chars long').bail(),
     body('url').isString().withMessage('Has to be a string').trim().isURL().withMessage('Must be a url').bail(),
-    body('comment').optional().isString().withMessage('has to be a string with any numbers and letters').custom(async (value: string) => {
-        const comment = [null, value]
-        if (comment) {
-            return comment
-        }
-    }).withMessage('has to be a string').trim().isLength({min:3}).withMessage('Has to be atleast 3 chars long').bail(),  
+    body('comment').optional().isString().withMessage('has to be a string with any numbers and letters').trim().isLength({min:3}).withMessage('Has to be atleast 3 chars long').bail(),  
 ]
 
 export const updatingPhotoRules = [
     body('title').isString().withMessage('Has to be a string').trim().isLength({min: 3}).withMessage('has to be atleast 3 chars long').bail(),
     body('url').isString().withMessage('Has to be a string').trim().isURL().withMessage('Must be a url').bail(),
-    body('comment').optional().isString().withMessage('has to be a string with any numbers and letters').custom(async (value: string) => {
-        const comment = [null, value]
-        if (comment) {
-            return comment
-        }
-    }).withMessage('has to be a string').trim().isLength({min:3}).withMessage('Has to be atleast 3 chars long').bail(),  
+    body('comment').optional().isString().withMessage('has to be a string with any numbers and letters').trim().isLength({min:3}).withMessage('Has to be atleast 3 chars long').bail(),  
 ]
