b4e05d8654aae81411598705bb3b451935ea7c2e
Patrik He <patrikswgit@gmail.com>
2023-02-17 02:34:56 +0100

    added sanitize and validation rules for updatingalbumrules and addphotorules. also added sanitize for createalbumrules

--- src/validations/album_rules.ts
+++ src/validations/album_rules.ts
@@ -1,5 +1,13 @@
 import {body} from 'express-validator'
 
 export const createAlbumRules = [
-    body('title').isString().withMessage('Has to be a string').isLength({min: 3}).withMessage('has to be atleast 3 chars long').bail(),
+    body('title').isString().withMessage('Has to be a string').trim().isLength({min: 3}).withMessage('has to be atleast 3 chars long').bail(),
+]
+
+export const updatingAlbumRules = [
+    body('title').isString().withMessage('Has to be a string').trim().isLength({min: 3}).withMessage('has to be atleast 3 chars long').bail(),
+]
+
+export const addPhotoRules = [
+    body('photoId').isInt().withMessage('has to be a integer').trim().bail(),
 ]
