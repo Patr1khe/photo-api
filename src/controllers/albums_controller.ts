4689d0b6951046dbff5e7a592acc5411272e79f9
Patrik He <patrikswgit@gmail.com>
2023-02-21 09:01:10 +0100

    added unauthorized for message status code

--- src/controllers/albums_controller.ts
+++ src/controllers/albums_controller.ts
@@ -164,7 +164,7 @@ export const removePhoto = async ( req: Request, res: Response) => {
             }
         })
     } catch (err) {
-        return res.status(401).send({ message: "not found, this is not your ACCOUNT!" })
+        return res.status(401).send({ message: "not found, this is not your ACCOUNT, Account is unauthorized!" })
     }
 
     try {
